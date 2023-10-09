0d351c1b1736e3768ea12330fb4d3e45bb7ea5bc
ZKContract <qb@polyhedra.network>
2023-10-09 10:42:25 +0800

    update

--- solidity_project/contracts/verify.sol
+++ solidity_project/contracts/verify.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "./register.sol";
 
 contract Verify {
+    using ECDSA for bytes32;
+
     event RandomNumberRecorded(bytes32 randomNumber);
     event Committeed(bytes32 messageHash, address applicationAddress);
 
@@ -35,13 +38,11 @@ contract Verify {
         address applicationAddress,
         bytes32[] memory messageHashes,
         bytes[] memory signatures,
-        uint8[] memory v,
         bytes32[] memory expectedRandoms
     ) external returns (bool) {
         require(
             messageHashes.length == signatures.length &&
-            signatures.length == v.length &&
-            v.length == expectedRandoms.length,
+            signatures.length == expectedRandoms.length &&
             "Invalid input"
         );
         for (uint256 i = 0; i < messageHashes.length; i++) {
@@ -49,7 +50,6 @@ contract Verify {
                 applicationAddress,
                 messageHashes[i],
                 signatures[i],
-                v[i],
                 expectedRandoms[i]
             );
         }
@@ -60,7 +60,6 @@ contract Verify {
         address applicationAddress,
         bytes32 messageHash,
         bytes memory signature,
-        uint8 v,
         bytes32 expectedRandom
     ) public returns (bool) {
         require(
@@ -70,7 +69,12 @@ contract Verify {
         bytes memory registeredKey = registerContract.getPublicKey(
             applicationAddress
         );
-        address signer = recoverSigner(messageHash, signature, v);
+        require(
+            registeredKey.length != 0,
+            "Application not registered"
+        );
+
+        address signer = messageHash.toEthSignedMessageHash().recover(signature);
         require(
             signer == publicKeyToAddress(registeredKey),
             "Invalid signature"
@@ -89,42 +93,5 @@ contract Verify {
             mstore(0, hash)
             addr := mload(0)
         }
-    }
-
-    function recoverSigner(
-        bytes32 messageHash,
-        bytes memory sig,
-        uint8 v
-    ) internal pure returns (address) {
-        (bytes32 r, bytes32 s) = splitSignature(sig);
-        return ecrecover(messageHash, v, r, s);
-    }
-
-    function splitSignature(
-        bytes memory sig
-    ) internal pure returns (bytes32 r, bytes32 s) {
-        // Check the signature length
-        // - case 65: r,s,v signature (standard)
-        // - case 64: r,vs signature (cf https://eips.ethereum.org/EIPS/eip-2098)
-        if (sig.length == 65) {
-            // ecrecover takes the signature parameters, and the only way to get them
-            // currently is to use assembly.
-            // solhint-disable-next-line no-inline-assembly
-            assembly {
-                r := mload(add(sig, 0x20))
-                s := mload(add(sig, 0x40))
-            }
-        } else if (sig.length == 64) {
-            // ecrecover takes the signature parameters, and the only way to get them
-            // currently is to use assembly.
-            // solhint-disable-next-line no-inline-assembly
-            assembly {
-                r := mload(add(sig, 0x20))
-                s := mload(add(sig, 0x40))
-            }
-        } else {
-            revert("ECDSA: invalid signature length");
-        }
-        return (r, s);
     }
 }
